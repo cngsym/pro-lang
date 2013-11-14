cb4221ac4ee938c35f9280b45a99e5e10ab2b19e
cngsym <seyma.ibis@bil.omu.edu.tr>
2013-11-15 00:53:27 +0200

    face detection

---
+++ Matlab/face_detection/face_detection.m
@@ -0,0 +1,58 @@
+clear all; close all;
+I=imread('harry_potter.jpg');
+
+
+
+% asagida renk donusumu yapisi olusturuyoruz ve  renklerin daha keskin hale
+% gelmesini sagliyoruz 
+
+cform = makecform('srgb2lab'); 
+yeni_form  = applycform(I,cform) ; % figure(2);imshow(yeni_form);
+
+
+
+katman2 = yeni_form(:,:,2);  % figure(3);imshow(katman1);
+level1 =graythresh(yeni_form(:,:,2));
+bw1=im2bw(yeni_form(:,:,2),level1);  %figure(4);imshow(BW1);
+
+
+
+katman3 = yeni_form(:,:,3);% figure(5);imshow(katman3);
+
+level2=graythresh(yeni_form(:,:,3));
+                                      
+bw2=im2bw(yeni_form(:,:,3),level2); % figure(6);imshow(bw2)
+
+                                       
+
+bw=bw1.*bw2;
+
+bw = bwareaopen(bw,80);  % 80 pixelden kucuk olanlar kaldiriyoruz.
+bw = imfill(bw,'holes'); % figure(7);imshow(bw)
+
+c = regionprops(bw, 'Centroid');
+a = [c.Centroid];
+
+ % x ve y degerleri her bir yüzün agl?rl?k merkezinin x ve y
+ % koordinatlaridir
+j = 2;
+s = 1;
+
+figure(8) ;imshow(I)
+for i = 1:2:length(a)
+        x =  (a(i)-50)
+        y = (a(j)-50)
+        
+        Ii = imcrop(I,[x y 100 100]);
+        imwrite(Ii,num2str(s,'face_%02d.jpg'));
+        figure(i); imshow(Ii) 
+        
+        j = j+2;
+        s = s + 1;
+end
+
+
+  
+
+    
+
